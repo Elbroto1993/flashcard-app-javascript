17e0824577f514a6176335461ae9a6ec0f0330e2
mblesing <marcel_andre.blesing@smail.th-koeln.de>
2019-11-23 16:24:00 +0100

    Final Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Flashcard-App
+
+Website to learn with flashcards, based on an spaced-repition-system algorithm.
+The App is structured into a MVC architecture.
+The Backend only offers a Rest API, written in Golang. Therefore all the business logic lies on the client-side.
+
+![Alt text](./screenshots/Home.PNG?raw=true)
+![Alt text](./screenshots/KarteiKasten.PNG?raw=true?)
+![Alt text](./screenshots/Lernen.PNG?raw=true)
+
+## Built With
+
+- JavaScript
+- [Go](https://www.golang.org/) - The Go Programming Language
+- [Bootstrap](https://getbootstrap.com/) - CSS Framework
+
+## Author
+
+- **Marcel Blesing**
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_karteikarteRepo "github.com/Elbroto1993/web-ss19/app/controller/karteikarte/repository"
+	_karteikarteUcase "github.com/Elbroto1993/web-ss19/app/controller/karteikarte/usecase"
+	_karteikarteHttpDeliver "github.com/Elbroto1993/web-ss19/app/route/delivery_karte/http"
+
+	_karteikastenRepo "github.com/Elbroto1993/web-ss19/app/controller/karteikasten/repository"
+	_karteikastenUcase "github.com/Elbroto1993/web-ss19/app/controller/karteikasten/usecase"
+	_karteikastenHttpDeliver "github.com/Elbroto1993/web-ss19/app/route/delivery_kasten/http"
+
+	_sessionHttpDeliver "github.com/Elbroto1993/web-ss19/app/controller/session"
+	_userRepo "github.com/Elbroto1993/web-ss19/app/controller/user/repository"
+	_userUcase "github.com/Elbroto1993/web-ss19/app/controller/user/usecase"
+	_userHttpDeliver "github.com/Elbroto1993/web-ss19/app/route/delivery_user/http"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/braintrain?parseTime=true")
+	if err != nil {
+		fmt.Println(err)
+	}
+	db.SetMaxIdleConns(10)
+	defer db.Close()
+
+	router := mux.NewRouter()
+
+	// Subrouter setup for user api
+	userRepo := _userRepo.NewMysqlUserRepository(db)
+	userUsecase := _userUcase.NewUserUsecase(userRepo)
+	userPrefix := router.PathPrefix("/user").Subrouter()
+	_userHttpDeliver.NewUserHttpHandler(userPrefix, userUsecase)
+
+	// Subrouter setup for karteikasten api
+	karteikastenRepo := _karteikastenRepo.NewMysqlKarteikastenRepository(db)
+	karteikastenUsecase := _karteikastenUcase.NewKarteikastenUsecase(karteikastenRepo)
+	karteikastenPrefix := router.PathPrefix("/karteikasten").Subrouter()
+	_karteikastenHttpDeliver.NewKarteikastenHttpHandler(karteikastenPrefix, karteikastenUsecase)
+
+	// Subrouter setup for karteikarte api
+	karteikarteRepo := _karteikarteRepo.NewMysqlKarteikarteRepository(db)
+	karteikarteUsecase := _karteikarteUcase.NewKarteikarteUsecase(karteikarteRepo)
+	karteikartePrefix := router.PathPrefix("/karteikarte").Subrouter()
+	_karteikarteHttpDeliver.NewKarteikarteHttpHandler(karteikartePrefix, karteikarteUsecase)
+
+	userPrefix = router.PathPrefix("/login").Subrouter()
+	_sessionHttpDeliver.NewSessionHttpHandler(userPrefix, userUsecase)
+
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	log.Fatal(http.ListenAndServe("localhost:8080", router))
+}
